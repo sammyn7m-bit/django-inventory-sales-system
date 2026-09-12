81328fd860b131ddc2e419bce4340cb5ec0f7fbc
sammyn7m-bit <sammyn7m@gmail.com>
2026-09-12 23:47:13 +0300

    Update core URLs configuration

--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('inventory.urls')),
+    path('sales/', include('sales.urls')),
 ]
